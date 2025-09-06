144ac1cc913b26f59ff049e624056e8627e60b0f
AntonioYoma96 <yomaraddatz96@gmail.com>
2025-09-06 12:24:21 -0400

    Arreglando formulario 3

--- index.js
+++ index.js
@@ -6,23 +6,4 @@
       $iframe.attr('src', $iframe.attr('src'));
     });
   });
-
-  document.getElementById("contacto-form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Evita la recarga de página
-
-  // Captura de datos y envío usando fetch
-  const form = event.target;
-  const data = new FormData(form);
-
-  fetch("/", {
-    method: "POST",
-    body: data
-  })
-  .then(() => {
-    // Muestra mensaje de confirmación
-    document.getElementById("mensaje-confirmacion").style.display = "block";
-    // Limpia el formulario
-    form.reset();
-  })
-  .catch((error) => alert("Ocurrió un error al enviar el formulario: " + error));
-});
+  
